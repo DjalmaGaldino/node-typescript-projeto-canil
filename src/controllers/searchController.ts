0334abf5d81696693be520aa68093088b8667dcd
Djalma Galdino <galdino.dj@gmail.com>
2022-02-23 10:17:39 -0300

    projeto final 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+
 // minha primeira rota para ser importada no index routes
 export const search = (req: Request, res: Response) => {
+   
+    let query: string = req.query.q as string;
 
-    // res.render('pages/search');
+    // se não tiver nada na busca, volta par home:
+        if (!query) {
+            res.redirect('/');
+            return;
+        }
 
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ server.use(mainRoutes);
 
 // configurando uma pagina de error
 server.use((req, res) => {
-    res.send('pagina não encontrada!')
+    res.render('pages/404')
 })
 
 // ja tenho um arquivo na porta 4000, por isso devo deixar na 80 agora, senao não roda
